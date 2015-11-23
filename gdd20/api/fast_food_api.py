3a13293fb9d523d9f7637da1b2618dea220e44c8
Rodrigo Araújo <rodrigo.araujo@jusbrasil.com.br>
2015-11-22 22:32:16 -0300

    login/register fully developed

--- gdd20/api/fast_food_api.py
+++ gdd20/api/fast_food_api.py
@@ -27,6 +27,7 @@ class FastFoodClient(RESTClientJSONBase):
     def get_recipe_by_items(self, items, restrict):
         return self.get('/recipes/items/?items={}&restrict={}'.format(items, restrict)).data
 
+    # TODO: handle error gracefully
     def register_new_user(self, login, password, role, name):
         data = {'login': login,
                 'password': password,
@@ -47,10 +48,11 @@ class FastFoodClient(RESTClientJSONBase):
     def load_user(id):
         data = {'user_id': id}
         req = json.loads(requests.get(api_url+'/users/', data=data).text)
+
         if not req:
             return None
-        # if req, then build the object and return it
-        return None     # THIS IS TEMPORARY!!!!!!!!
-
-
-
+        user = User(id=id, name=req['name'],
+                    role=req['role'],
+                    email=req['email'],
+                    token=req['password'])
+        return user
