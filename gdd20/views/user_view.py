1ef4f549be2088a403990571b100916437233d0e
Rodrigo Araújo <rodrigo.araujo@jusbrasil.com.br>
2015-12-07 18:50:41 -0300

    bring recipes liked by the user

--- gdd20/views/user_view.py
+++ gdd20/views/user_view.py
@@ -2,7 +2,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from gdd20.api.fast_food_api import FastFoodClient
 from flask.ext.login import current_user
-from gdd20 import app
 
 
 user_blueprint = Blueprint('user_blueprint', __name__, template_folder='templates')
@@ -11,6 +10,13 @@ fast_food = FastFoodClient()
 
 @user_blueprint.route('/profile/', methods=['GET'])
 def profile():
-	if not current_user.is_authenticated:
-		redirect('/')
-	return render_template('user/profile.html', title='Profile')
+    if not current_user.is_authenticated:
+        redirect('/')
+    ids = fast_food.get_user_likes(current_user.id)
+    liked_recipes = []
+    for id in ids:
+        liked_recipes.append(fast_food.get_recipe_by_id(id))
+
+    return render_template('user/profile.html', title='Profile',
+                           liked_recipes=liked_recipes)
+    
